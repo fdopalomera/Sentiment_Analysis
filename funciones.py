53f4ea0baaee18938df66b3f2257953e8cc0ed3e
Fernando Palomera <fernando@MacBook-Pro-de-Fernando.local>
2020-03-26 02:16:13 -0300

    added pipelines for serialization

--- funciones.py
+++ funciones.py
@@ -1,10 +1,14 @@
 import re
 import string
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.stem import PorterStemmer
 from nltk.stem import WordNetLemmatizer
 from sklearn.metrics import roc_auc_score, confusion_matrix, classification_report
+from sklearn.model_selection import GridSearchCV
 from time import time
 
 
@@ -67,10 +71,18 @@ def clf_metrics(clf, X_train, y_train, X_test, y_test):
 
     """    
     tic = time()
+   # Corrroboración preproceso target
+    if (y_train.dtype =='object') & (y_test.dtype == 'object'):
+
+        lbl_encoder = LabelEncoder()
+        y_train = lbl_encoder.fit_transform(y_train)
+        y_test = lbl_encoder.transform(y_test)
+
     # Entrenar el modelo
     clf.fit(X_train, y_train)
-    # Imprimir mejores parámetros
-    print(clf.best_params_)
+    # Imprimir mejores parámetros sí el objeto 
+    if isinstance(clf, GridSearchCV):
+        print(clf.best_params_)
     # Predecir la muestra de validación
     y_hat = clf.predict(X_test)
     # Métricas
@@ -80,3 +92,47 @@ def clf_metrics(clf, X_train, y_train, X_test, y_test):
     for key, value in metrics.items():
         print('{}:\n{}'.format(key, value))
     return print("Realizado en {:.3f}s".format(time() - tic))
+
+
+def compare_classifiers(estimators, X_test, y_test, n_cols=2):
+
+    """
+    Compara en forma gráfica las métricas de clasificación a partir de una lista de 
+    tuplas con los modelos (nombre_modelo, modelo_entrendo) 
+    """
+
+    rows = np.ceil(len(estimators)/n_cols)
+    height = 2 * rows
+    width = n_cols * 5
+    fig = plt.figure(figsize=(width, height))
+
+    colors = ['dodgerblue', 'tomato', 'purple', 'orange']
+
+    for n, model in enumerate(estimators):
+
+        y_hat = model[1].predict(X_test) 
+        dc = classification_report(y_test, y_hat, output_dict=True)
+
+        plt.subplot(rows, n_cols, n + 1)
+
+        for i, j in enumerate(['0', '1', 'macro avg']):
+
+            tmp = {'0': {'marker': 'x', 'label': f'Class: {j}'},
+                   '1': {'marker': 'x', 'label': f'Class: {j}'},
+                   'macro avg': {'marker': 'o', 'label': 'Avg'}}
+
+            plt.plot(dc[j]['precision'], [1], marker=tmp[j]['marker'], color=colors[i])
+            plt.plot(dc[j]['recall'], [2], marker=tmp[j]['marker'], color=colors[i])
+            plt.plot(dc[j]['f1-score'], [3], marker=tmp[j]['marker'],color=colors[i], label=tmp[j]['label'])
+            plt.axvline(x=.5, ls='--')
+
+        plt.yticks([1.0, 2.0, 3.0], ['Precision', 'Recall', 'f1-Score'])
+        plt.title(model[0])
+        plt.xlim((0.1, 1.0))
+
+        if (n + 1) % 2 == 0:
+            plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+            
+    fig.tight_layout()
+    
+    return
